216f76b6e5ad419e0aede7dc33972f0469663c27
Sebastian Rein <sebastian.rein@tum.de>
2026-02-25 15:22:15 +0100

    added missing pyarrow requirement

--- src/eulexbuild/__init__.py
+++ src/eulexbuild/__init__.py
@@ -1,7 +1,7 @@
 from .EULEXBuildPipeline import EULEXBuildPipeline
 from .config_validation import validate_configuration
 
-__version__ = "0.2.2"
+__version__ = "0.2.3"
 
 __all__ = [
     "validate_configuration",
